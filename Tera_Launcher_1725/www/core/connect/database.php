9375978066bcd4dff1c9708217ad33cce35d7bf2
Jamezz <di.jamezz@gmail.com>
2013-09-28 16:56:18 -0700

    Added port selection to database configuration.

--- Tera_Launcher_1725/www/core/connect/database.php
+++ Tera_Launcher_1725/www/core/connect/database.php
@@ -1,10 +1,11 @@
 <?php 
 $config = array(
-	'host'		=> 'localhost',
+	'host'		=> '127.0.0.1',	//Must be IP or FQDN. Don't use localhost or it'll use a UNIX Socket.
+	'port'		=> '3306',
 	'username' 	=> 'root',
 	'password' 	=> 'PASSWORD',
 	'dbname' 	=> 'tera'
 );
 
-$db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password']);
+$db = new PDO('mysql:host=' . $config['host'] . ';port=' . $config['port'] .	';dbname=' . $config['dbname'], $config['username'], $config['password']);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
